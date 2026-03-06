dd18b81ddcebb55fdae125f8f35419260fe67dfc
毛民9527 <17385705388@163.com>
2026-03-06 14:09:43 +0800

    Add YOLO detection script with default parameters

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+set -e
 
+WEIGHTS=${1:-runs/ppe_baseline/weights/best.pt}
+SOURCE=${2:-data/images}   # 你可以换成一张图片/一个文件夹/一个视频
+
+yolo detect predict \
+  model="$WEIGHTS" \
+  source="$SOURCE" \
+  imgsz=640 \
+  conf=0.25 \
+  save=True
