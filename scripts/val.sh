12b9e9ca4ab78c6354a0985a9030575c37ad4094
毛民9527 <17385705388@163.com>
2026-03-06 14:09:09 +0800

    Add validation script for YOLO detection

--- scripts/val.sh
+++ scripts/val.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+set -e
 
+WEIGHTS=${1:-runs/ppe_baseline/weights/best.pt}
+DATA=${2:-configs/ppe.yaml}
+
+yolo detect val \
+  model="$WEIGHTS" \
+  data="$DATA" \
+  imgsz=640
